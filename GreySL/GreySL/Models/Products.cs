17e73eba79342606a5fe4c99c64af6bb8003384d
chidi1 <chidi@lotusbetaanalytics.com>
2018-02-20 13:43:48 +0100

    Initial commit

---
+++ GreySL/GreySL/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreySL.Models
+{
+    public class Products
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+        [Display(Name = "Price (₦)")]
+        public decimal price { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Product Image ")]
+        public string ImageUrl { get; set; }
+
+    }
+}
